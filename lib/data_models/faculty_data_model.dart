98c5bb77f3da62bab1915e07f9cfd68d71339bf0
Wachiraya <96040259+Wachiice@users.noreply.github.com>
2023-12-27 18:15:15 +0700

    Delete lib directory

--- lib/data_models/faculty_data_model.dart
+++
@@ -1,5 +0,0 @@
-class FacultyDataModel {
-  final String facultyName, facultyLink, facultyNameThai, imageFile;
-  FacultyDataModel(
-      this.facultyName, this.facultyLink, this.facultyNameThai, this.imageFile);
-}
